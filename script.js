266740448318e2011473e5863c59cf5f8dca6c82
Ruvin12 <ruvimy8@gmail.com>
2020-02-27 21:01:03 -0800

    added events

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#shounen").on("click", function() {
+
+});
+$("#supernatural").on("click", function() {
+
+});
+$("#slice of life").on("click", function() {
+
+});
+$("#romance").on("click", function() {
+
+});
+$("#magic").on("click", function() {
+  
+});
