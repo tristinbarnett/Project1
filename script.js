59002a8a8b009ba2ba5f94c3687d6c7011387652
Ruvin12 <ruvimy8@gmail.com>
2020-03-03 20:27:20 -0800

    added value

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 
 
 $(document).ready(function(){
-    $("#shounen").on("click", function() {
+    
 
-    });
-    $("#supernatural").on("click", function() {
-    
-    });
-    $("#slice of life").on("click", function() {
-    
-    });
-    $("#romance").on("click", function() {
-    
-    });
-    $("#magic").on("click", function() {
-      
-    });
     
     $(".button").click(function(){
         var anime = $(".input").val();
