ca37e2d1e4a5dcc00c23ea4e980d1b8b1d9ed152
Tristin Barnett <tristinbarnett@gmail.com>
2020-03-07 09:58:35 -0800

    added files from forked project

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Project1
+# Otaku Zone
+With this project we were placed into teams of three and asked to use our newly acquired skills and knowledge to build a web application from scratch. Working collaboratively, we learned agile development methodologies and implemented feature and bug fixes using the git branch workflow and pull requests.  Integrating data received from multiple server-side API requests we created a page that solved a problem.
+
+## Challenges
+This was our first go at working in teams and using branches with git hub. It was very challenging going at it in a trail and error approach to see how best to divide the work and where to begin. Communication and remembering that we're all working on the same file and trying to keep track of where everyone is writing was very challenging. This is definitely something that experience will help with getting used to. 
+
+## User Story
+Given I am an anime fan
+When I search for recommendations
+Then I git a list of recommendations and know if there is a manga to go with the anime
+
+## Deployed Project
+
+[Otaku Zone](https://tristinbarnett.github.io/Project1/)
+
+![alt text](otakuZone.png "Otaku Zone")
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
+var responseArray;
+$(document).ready(function () {
+    
     // check if genre buttons are clicked
-    $(".button").click(function(){
+    $(".buttonGenre").click(function(){
         var d = $(this).data("value")
         console.log(d)
         var genreURL ="https://api.jikan.moe/v3/genre/anime/"+d+"/1"
@@ -21,63 +23,128 @@ $(document).ready(function(){
         
     });
 
-})
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://cors-anywhere.herokuapp.com/https://community-manga-eden.p.rapidapi.com/list/0",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "community-manga-eden.p.rapidapi.com",
+            "x-rapidapi-key": "006e809b87msh0c1cbdaccb16659p12b8d5jsn35e1032a8f6a",
+            "Access-Control-Allow-Origin": "*"
+        }
+    }
+    //calling response from manga api
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        responseArray = response;
+    });
 
 
-function search(queryURL){
-    $.ajax({
-            url: queryURL,
-            method: "GET"
+});
+
+    function genre(queryURL){
+        $.ajax({
+            url:queryURL,
+            method:"GET"
         }).then(function(response){
-            console.log(response);
-            $("#searchResults").html(" ");
             $("#recommendations").html("");
-            $.each(response.results,function(index,value){
-                $("#searchResults").append(`<div onclick="ID(${response.results[index].mal_id})"  class="animeName" value=${response.results[index].mal_id} > ${response.results[index].title}</div>`)
-            });
+            $.each(response.anime,function(index,value){
+                $("#recommendations").append(`<div onclick="ID(${response.anime[index].mal_id},'${response.anime[index].title}')" class='animeName' value='${response.anime[index].mal_id}'>  ${response.anime[index].title}</div>`);
+                console.log(response.anime[index].title);
+            })
         })
+    };
 
-}
-
-function genre(queryURL){
-    $.ajax({
-        url:queryURL,
-        method:"GET"
-    }).then(function(response){
+    // ajax call with anime id
+    function ID(animeID, mangaTitle) {
         $("#recommendations").html("");
-        $.each(response.anime,function(index,value){
-            $("#recommendations").append(`<div onclick="ID(${response.anime[index].mal_id})" class='animeName' value='${response.anime[index].mal_id}'>  ${response.anime[index].title}</div>`)
-            
-        })
-    })
-}
-
-// ajax call with anime id
-function ID(animeID){
-        $("#recommendations").html("");
-        var queryIDURL = "https://api.jikan.moe/v3/anime/"+animeID+"/"
+        var queryIDURL = "https://api.jikan.moe/v3/anime/" + animeID + "/"
         $.ajax({
             url: queryIDURL,
             method: "GET"
-        }).then(function(response){
-            console.log(response)
+        }).then(function (response) {
             // clear searchResults
             $("#searchResults").html("");
             // append selected anime
             $("#searchResults").append(`<div class='animeName' value='${response.title}'> ${response.title}</div>`)
             $("#searchResults").append(`<a href= '${response.url}' class ='cards' style='float:left, box-sizing:border-box'><div class='card_image'> <img src='${response.image_url}'></div> </a>`);
             $("#searchResults").append(response.synopsis)
-            // append recommendation 
+            
         })
         // Ajax call for recommendations 
-        var queryIDURL = "https://api.jikan.moe/v3/anime/"+animeID+"/recommendations"
+        var queryIDURL = "https://api.jikan.moe/v3/anime/" + animeID + "/recommendations"
         $.ajax({
             url: queryIDURL,
             method: "GET"
         }).then(function(response){    
             $.each(response.recommendations,function(index,value){
-            $("#recommendations").append(`<div class='animeName' onclick="ID(${response.recommendations[index].mal_id})" value=${response.recommendations[index].mal_id}'> ${response.recommendations[index].title}</div>`)
+            $("#recommendations").append(`<div class='animeName' onclick="ID(${response.recommendations[index].mal_id},'${response.recommendations[index].title}')" value=${response.recommendations[index].mal_id}'> ${response.recommendations[index].title}</div>`)
             })
         });
-    
-}
+
+        console.log("title is "+mangaTitle);
+
+        //getting manga with matching title
+        var matchedElementsArray = responseArray.manga.filter(function (item) {
+            return item.t.toLowerCase().replace(/\s/g, '') === mangaTitle.toLowerCase().replace(/\s/g, '');
+        })
+        var mangaId;
+        if (matchedElementsArray.length > 0) {
+            console.log(matchedElementsArray);
+            mangaId = matchedElementsArray[0].i;
+        }
+        console.log(mangaId);
+        if (mangaId) {
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://cors-anywhere.herokuapp.com/https://www.mangaeden.com/api/manga/" + mangaId + "/",
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "community-manga-eden.p.rapidapi.com",
+                    "x-rapidapi-key": "006e809b87msh0c1cbdaccb16659p12b8d5jsn35e1032a8f6a"
+                }
+            }
+            
+            $.ajax(settings).then(function (response) {
+                console.log(response);
+                $("#noManga").empty();
+                if (response.url) {
+                    mangaLink = $("<a>").attr("href", response.url);
+                    mangaLink.text("Click to see manga!");
+                    $("#noManga").append(mangaLink);
+                } else {
+                    var noURL = $("<p>").text("There's a Manga!");
+                    $("#noManga").append(noURL);
+                }
+            });
+        } else {
+            $("#noManga").empty();
+            var noMangaText = $("<p>").text("There's no Manga for this Anime!");
+            console.log(noMangaText);
+            $("#noManga").append(noMangaText);
+        };
+    };
+
+
+    function search(queryURL) {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            $("#searchResults").html(" ");
+            $("#recommendations").html("");
+            $("#noManga").empty();
+            $.each(response.results, function (index, value) {
+                $("#searchResults").append(`<div class="animeName${index}" value=${value.mal_id}> ${value.title}</div>`);
+                $(".animeName" + index).on("click", function () {
+                    ID(value.mal_id, value.title);
+                })
+            });
+
+        })
+
+    }
+
